7ee836012f301b98d0683d8477c0489a650e65d4
malinowski <malinowski.dustin@gmail.com>
2016-10-24 22:21:02 -0400

    created main stylesheet and started playing with requestAnimationFrame

--- js/core.js
+++ js/core.js
@@ -1,3 +1,31 @@
+var charContainer = document.createElement("div");
+var characters=[];
+characters[0] = "Dustin";
+characters[1] = "Bryan";
+characters[2] = "Matt";
+characters[3] = "Tom";
+characters[4] = "Ian";
+var tempCounter = 0;
+var blocksCreated = 0;
+
 function init(){
-	alert("hello world");
+	charContainer.setAttribute("id", "charContainer");
+	document.getElementsByTagName("main")[0].appendChild(charContainer);
+	charContainer = document.getElementById("charContainer");
+	requestAnimationFrame(addCharBlock);
+}
+
+function addCharBlock(){
+	if(blocksCreated < characters.length){
+		if(tempCounter >= .5){
+			var charBlock = document.createElement("div");
+			charBlock.setAttribute("class", "charBlock");
+			charContainer.appendChild(charBlock);
+			blocksCreated++;
+			tempCounter = 0;
+		}else{
+			tempCounter+=.01;			
+		}  
+		requestAnimationFrame(addCharBlock);
+	}
 }
